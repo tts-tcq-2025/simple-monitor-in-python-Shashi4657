bab2806b9cf13a0265d2692dc81cc2e7580976d9
Shashi4657 <bsshashigowda@gmail.com>
2025-07-31 09:31:54 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,25 +1,25 @@
 
-def is_temperature_ok(temperature):
-  return 0 <= temperature <= 45
+def is_in_range(value, min_val, max_val):
+  return min_val <= value <= max_val
 
-def is_soc_ok(soc):
-  return 20 <= soc <= 80
-
-def is_charge_rate_ok(charge_rate):
-  return charge_rate <= 0.8
+def is_below_max(value, max_val):
+  return value <= max_val
 
 def battery_is_ok(temperature, soc, charge_rate):
-  if not is_temperature_ok(temperature):
-    print('Temperature is out of range!')
-    return False
-  if not is_soc_ok(soc):
-    print('State of Charge is out of range!')
-    return False
-  if not is_charge_rate_ok(charge_rate):
-    print('Charge rate is out of range!')
-    return False
+  checks = [
+    {"name": "Temperature", "value": temperature, "check": is_in_range, "args": (0, 45)},
+    {"name": "State of Charge", "value": soc, "check": is_in_range, "args": (20, 80)},
+    {"name": "Charge Rate", "value": charge_rate, "check": is_below_max, "args": (0.8,)}
+  ]
+
+  for check in checks:
+    if not check["check"](check["value"], *check["args"]):
+      print(f'{check["name"]} is out of range!')
+      return False
   return True
 
 if __name__ == '__main__':
-  assert(battery_is_ok(25, 70, 0.7) is True)
-  assert(battery_is_ok(50, 85, 0) is False)
+  assert(battery_is_ok(25, 70, 0.7) is True)     # all OK
+  assert(battery_is_ok(50, 70, 0.7) is False)    # temperature too high
+  assert(battery_is_ok(25, 10, 0.7) is False)    # soc too low
+  assert(battery_is_ok(25, 70, 0.9) is False)    # charge rate too high
